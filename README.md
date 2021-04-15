d8530653b08497ced3ab31b5b8f6be91192db377
Kinyugo <kinyugokyde@gmail.com>
2021-04-15 16:21:41 +0300

    added a script to download checkpoints and updated README with instructions on how to run the scripts

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 
 ## Setup
 
-To compare results of various methods execute
-
 ```bash
 $ pip install -r requirements.txt
+```
+
+## Fetching Checkpoints
+
+```bash
+$ python fetch_checkpoints.py
+```
+
+## Running
+
+To visualize how **_Bicubic Interpolation_**, **_SRResNet_** and **_SRGAN_** perform, run:
+
+> To change the image used modify the block under `main` in **super_resolve.py**
+
+```bash
 $ python super_resolve.py
 ```
---
+++ fetch_checkpoints.py
@@ -0,0 +1,51 @@
+import os
+
+import requests
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+if __name__ == "__main__":
+    os.makedirs("./checkpoints", exist_ok=True)
+
+    # Download checkpoints from google drive
+    # https://drive.google.com/file/d/1--FmF5_fR8v9h8dngJ44DtcowpMX4vPp/view?usp=sharing
+    ssrresnet_id = "1--FmF5_fR8v9h8dngJ44DtcowpMX4vPp"
+    ssrresnet_dest = "./checkpoints/checkpoint_srresnet.pth.tar"
+
+    # https://drive.google.com/file/d/16MygTm9Ba1rmQfldOyGU8zY-Ike6E9qa/view?usp=sharing
+    ssrgan_id = "16MygTm9Ba1rmQfldOyGU8zY-Ike6E9qa"
+    ssrgan_dest = "./checkpoints/checkpoint_srgan.pth.tar"
+
+    download_file_from_google_drive(ssrresnet_id, ssrresnet_dest)
+    download_file_from_google_drive(ssrgan_id, ssrgan_dest)
